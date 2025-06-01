8d20d8a410d054eb925e8cb3e20576de22cfe314
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-01 11:24:46 +0545

    added q5

---
+++ Week 01/Q5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+//Author: Sharwan jung kunwar
+//Purpose: To find volume of a cube.
+
+int main(){
+    int a =2;
+
+    int formula = a*a*a;
+
+    printf("The volume of cube is : %d\n",formula);
+
+    return 0;
+}
