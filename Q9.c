75014b5369eb26161615468883d7e3d942e849be
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-01 11:35:13 +0545

    added Q9

---
+++ Q9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//Author: Sharwan jung kunwar
+//Purpose: To find surface area of sphere.
+
+int main(){
+    int r =5;
+    float sa = 4*3.14*r*r;
+
+    printf("The surface area of sphere is %.2f", sa);
+
+    return 0;
+}
