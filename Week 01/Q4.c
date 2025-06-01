ee88bcf6951a55323b1fe9e1fee2974b2a28ec3a
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-01 11:22:53 +0545

    added q4

---
+++ Week 01/Q4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//Author : Sharwan jung kunwar
+//Purpose: to find product of two floating point nubmers.
+
+int main(){
+    float num01 = 10.5;
+    float num02 = 20.7;
+
+    float product = num01*num02;
+
+    printf("The product of %.3f and %.3f is %.3f\n", num01, num02, product);
+
+
+    return 0;
+}
